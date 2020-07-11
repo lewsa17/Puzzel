83095ff9870f5d696a9da99bf5a6c5c728b0af25
lewsa17@gmail.com <lewsa17@gmail.com>
2020-07-11 19:31:34 +0200

    Poprawiono nazwy kontrolek

--- Forms/External/LAPSui.Designer.cs
+++ Forms/External/LAPSui.Designer.cs
@@ -29,99 +29,99 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LAPSui));
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.labelCompName = new System.Windows.Forms.Label();
+            this.inputedcomputerName = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textPasswordExpires = new System.Windows.Forms.TextBox();
+            this.labelPasswordExpires = new System.Windows.Forms.Label();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // labelCompName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 26);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nazwa Komputera";
+            this.labelCompName.AutoSize = true;
+            this.labelCompName.Location = new System.Drawing.Point(12, 26);
+            this.labelCompName.Name = "labelCompName";
+            this.labelCompName.Size = new System.Drawing.Size(94, 13);
+            this.labelCompName.TabIndex = 0;
+            this.labelCompName.Text = "Nazwa Komputera";
             // 
-            // textBox1
+            // inputedcomputerName
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 42);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(318, 20);
-            this.textBox1.TabIndex = 1;
+            this.inputedcomputerName.Location = new System.Drawing.Point(12, 42);
+            this.inputedcomputerName.Name = "inputedcomputerName";
+            this.inputedcomputerName.Size = new System.Drawing.Size(318, 20);
+            this.inputedcomputerName.TabIndex = 1;
             // 
-            // textBox2
+            // textPassword
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 96);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(318, 20);
-            this.textBox2.TabIndex = 3;
+            this.textPassword.Location = new System.Drawing.Point(12, 96);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.ReadOnly = true;
+            this.textPassword.Size = new System.Drawing.Size(318, 20);
+            this.textPassword.TabIndex = 3;
             // 
-            // label2
+            // labelPassword
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 80);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Hasło";
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(12, 80);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(36, 13);
+            this.labelPassword.TabIndex = 2;
+            this.labelPassword.Text = "Hasło";
             // 
-            // textBox3
+            // textPasswordExpires
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 136);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(318, 20);
-            this.textBox3.TabIndex = 5;
+            this.textPasswordExpires.Location = new System.Drawing.Point(12, 136);
+            this.textPasswordExpires.Name = "textPasswordExpires";
+            this.textPasswordExpires.ReadOnly = true;
+            this.textPasswordExpires.Size = new System.Drawing.Size(318, 20);
+            this.textPasswordExpires.TabIndex = 5;
             // 
-            // label3
+            // labelPasswordExpires
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 120);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Hasło wygasa";
+            this.labelPasswordExpires.AutoSize = true;
+            this.labelPasswordExpires.Location = new System.Drawing.Point(12, 120);
+            this.labelPasswordExpires.Name = "labelPasswordExpires";
+            this.labelPasswordExpires.Size = new System.Drawing.Size(75, 13);
+            this.labelPasswordExpires.TabIndex = 4;
+            this.labelPasswordExpires.Text = "Hasło wygasa";
             // 
-            // button1
+            // btnFind
             // 
-            this.button1.Location = new System.Drawing.Point(12, 172);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Wyszukaj";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.btnGetPwd);
+            this.btnFind.Location = new System.Drawing.Point(12, 172);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(156, 23);
+            this.btnFind.TabIndex = 6;
+            this.btnFind.Text = "Wyszukaj";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnGetPwd);
             // 
-            // button2
+            // btnClose
             // 
-            this.button2.Location = new System.Drawing.Point(174, 172);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(156, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Zamknij";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.btnClose);
+            this.btnClose.Location = new System.Drawing.Point(174, 172);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(156, 23);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Zamknij";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // LAPSui
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(334, 196);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.textPasswordExpires);
+            this.Controls.Add(this.labelPasswordExpires);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.inputedcomputerName);
+            this.Controls.Add(this.labelCompName);
             this.Icon = global::Forms.Resources.Resources.Puzzel;
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(350, 235);
@@ -137,13 +137,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelCompName;
+        private System.Windows.Forms.TextBox inputedcomputerName;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textPasswordExpires;
+        private System.Windows.Forms.Label labelPasswordExpires;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnClose;
     }
 }
--- Forms/External/LAPSui.cs
+++ Forms/External/LAPSui.cs
@@ -17,17 +17,17 @@ namespace Forms.External
         }
         public void LoadPassword()
         {
-            textBox1.Text = HostName;
-            textBox2.Text = CompPWD.GetPWD(HostName);
+            inputedcomputerName.Text = HostName;
+            textPassword.Text = CompPWD.GetPWD(HostName);
         }
-        private void btnClose(object sender, EventArgs e)
+        private void btnClose_Click(object sender, EventArgs e)
         {
             this.Dispose();
             Close();
         }
         private void btnGetPwd(object sender, EventArgs e)
         {
-            textBox2.Text = CompPWD.GetPWD(textBox1.Text);
+            textPassword.Text = CompPWD.GetPWD(inputedcomputerName.Text);
         }
 
         private void LAPSui_Load(object sender, EventArgs e)
