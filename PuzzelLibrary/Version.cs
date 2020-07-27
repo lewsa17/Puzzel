4f635a06993e94deb6744dbe51483e43104dd42a
lewsa17@gmail.com <lewsa17@gmail.com>
2020-07-27 08:15:18 +0200

    Podbicie wersji do 0.129.0

--- PuzzelLibrary/Version.cs
+++ PuzzelLibrary/Version.cs
@@ -3,7 +3,7 @@
     public static class Version
     {
         public static int Major { get => 0; }
-        public static int Minor { get => 128; }
+        public static int Minor { get => 129; }
         public static int Build { get => 0; }
         public static string GetVersion() => Major + "." + Minor + "." + Build;
     }
