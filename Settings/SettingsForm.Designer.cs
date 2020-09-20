c98e61e8f66a3b90360870b0e1f0bc91785b7244
lewsa17@gmail.com <lewsa17@gmail.com>
2020-09-20 22:29:34 +0200

    Pre-Modyfikator opisu

--- Settings/SettingsForm.Designer.cs
+++ Settings/SettingsForm.Designer.cs
@@ -30,13 +30,17 @@
         {
             this.SaveButton = new System.Windows.Forms.Button();
             this.CloseButton = new System.Windows.Forms.Button();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.TabSettings = new System.Windows.Forms.TabControl();
             this.GeneralPage = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.Other = new System.Windows.Forms.TabPage();
             this.DescriptionBox = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.tabControl1.SuspendLayout();
+            this.DescriptionLabel = new System.Windows.Forms.Label();
+            this.HistoryLogBox = new System.Windows.Forms.GroupBox();
+            this.CheckBoxHistoryLog = new System.Windows.Forms.CheckBox();
+            this.TabSettings.SuspendLayout();
+            this.GeneralPage.SuspendLayout();
             this.DescriptionBox.SuspendLayout();
+            this.HistoryLogBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // SaveButton
@@ -58,18 +62,19 @@
             this.CloseButton.UseVisualStyleBackColor = true;
             this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
             // 
-            // tabControl1
+            // TabSettings
             // 
-            this.tabControl1.Controls.Add(this.GeneralPage);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 3);
-            this.tabControl1.Name = "TabSettings";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(776, 406);
-            this.tabControl1.TabIndex = 2;
+            this.TabSettings.Controls.Add(this.GeneralPage);
+            this.TabSettings.Controls.Add(this.Other);
+            this.TabSettings.Location = new System.Drawing.Point(12, 3);
+            this.TabSettings.Name = "TabSettings";
+            this.TabSettings.SelectedIndex = 0;
+            this.TabSettings.Size = new System.Drawing.Size(776, 406);
+            this.TabSettings.TabIndex = 2;
             // 
             // GeneralPage
             // 
+            this.GeneralPage.Controls.Add(this.HistoryLogBox);
             this.GeneralPage.Location = new System.Drawing.Point(4, 24);
             this.GeneralPage.Name = "GeneralPage";
             this.GeneralPage.Padding = new System.Windows.Forms.Padding(3);
@@ -78,20 +83,20 @@
             this.GeneralPage.Text = "Ogólne";
             this.GeneralPage.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // Other
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 24);
-            this.tabPage2.Name = "Other";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(768, 378);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Inne";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.Other.Location = new System.Drawing.Point(4, 24);
+            this.Other.Name = "Other";
+            this.Other.Padding = new System.Windows.Forms.Padding(3);
+            this.Other.Size = new System.Drawing.Size(768, 378);
+            this.Other.TabIndex = 1;
+            this.Other.Text = "Inne";
+            this.Other.UseVisualStyleBackColor = true;
             // 
             // DescriptionBox
             // 
             this.DescriptionBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.DescriptionBox.Controls.Add(this.label1);
+            this.DescriptionBox.Controls.Add(this.DescriptionLabel);
             this.DescriptionBox.Location = new System.Drawing.Point(19, 303);
             this.DescriptionBox.Name = "DescriptionBox";
             this.DescriptionBox.Size = new System.Drawing.Size(762, 100);
@@ -99,14 +104,37 @@
             this.DescriptionBox.TabStop = false;
             this.DescriptionBox.Text = "Opis";
             // 
-            // label1
+            // DescriptionLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(275, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Najedź kursorem na opcję aby wyświetlić tutaj opis";
+            this.DescriptionLabel.AutoSize = true;
+            this.DescriptionLabel.Location = new System.Drawing.Point(3, 19);
+            this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(275, 15);
+            this.DescriptionLabel.TabIndex = 0;
+            this.DescriptionLabel.Text = "Najedź kursorem na opcję aby wyświetlić tutaj opis";
+            // 
+            // HistoryLogBox
+            // 
+            this.HistoryLogBox.Controls.Add(this.CheckBoxHistoryLog);
+            this.HistoryLogBox.Location = new System.Drawing.Point(20, 11);
+            this.HistoryLogBox.Name = "HistoryLogBox";
+            this.HistoryLogBox.Size = new System.Drawing.Size(127, 52);
+            this.HistoryLogBox.TabIndex = 0;
+            this.HistoryLogBox.TabStop = false;
+            this.HistoryLogBox.Text = "Historia logów";
+            // 
+            // CheckBoxHistoryLog
+            // 
+            this.CheckBoxHistoryLog.AutoSize = true;
+            this.CheckBoxHistoryLog.Location = new System.Drawing.Point(6, 22);
+            this.CheckBoxHistoryLog.Name = "CheckBoxHistoryLog";
+            this.CheckBoxHistoryLog.Size = new System.Drawing.Size(83, 19);
+            this.CheckBoxHistoryLog.TabIndex = 0;
+            this.CheckBoxHistoryLog.Text = "Wyłaczone";
+            this.CheckBoxHistoryLog.CheckedChanged += new System.EventHandler(this.ChangeChecked);
+            this.CheckBoxHistoryLog.MouseEnter += new System.EventHandler(this.MouseOn);
+            this.CheckBoxHistoryLog.MouseLeave += new System.EventHandler(this.MouseOut);
+            this.CheckBoxHistoryLog.UseVisualStyleBackColor = true;
             // 
             // SettingsForm
             // 
@@ -116,12 +144,15 @@
             this.Controls.Add(this.DescriptionBox);
             this.Controls.Add(this.CloseButton);
             this.Controls.Add(this.SaveButton);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.TabSettings);
             this.Name = "SettingsForm";
             this.Text = "Ustawienia";
-            this.tabControl1.ResumeLayout(false);
+            this.TabSettings.ResumeLayout(false);
+            this.GeneralPage.ResumeLayout(false);
             this.DescriptionBox.ResumeLayout(false);
             this.DescriptionBox.PerformLayout();
+            this.HistoryLogBox.ResumeLayout(false);
+            this.HistoryLogBox.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -130,11 +161,13 @@
 
         private System.Windows.Forms.Button SaveButton;
         private System.Windows.Forms.Button CloseButton;
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl TabSettings;
         private System.Windows.Forms.TabPage GeneralPage;
         private System.Windows.Forms.GroupBox DescriptionBox;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label DescriptionLabel;
+        private System.Windows.Forms.TabPage Other;
+        private System.Windows.Forms.GroupBox HistoryLogBox;
+        private System.Windows.Forms.CheckBox CheckBoxHistoryLog;
     }
 }
 
--- Settings/SettingsForm.cs
+++ Settings/SettingsForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Settings
@@ -18,10 +11,38 @@ namespace Settings
             if (ApplicationName != string.Empty)
                 this.Text = ApplicationName + " - " + this.Text;
         }
-
+        const string defaultDescription = "Najedź kursorem na opcję aby wyświetlić tutaj opis";
+        const string logsHistoryDescription = "Ustawienie tej opcji będzie wyświetlać lub nie ostatnio wyszukiwanych wartości";
         private void CloseButton_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void ChangeChecked(object sender, EventArgs e)
+        {
+            if (((CheckBox)sender).Checked)
+            {
+                ((CheckBox)sender).Text = "Włączone";
+            }
+            else ((CheckBox)sender).Text = "Wyłaczone";
+        }
+        private void MouseOn(object sender, EventArgs e)
+        {
+            var Name = ((CheckBox)sender).Name;
+            switch (Name)
+            {
+                case "CheckBoxHistoryLog": 
+                    {
+                        DescriptionLabel.Text = logsHistoryDescription; 
+                        break; 
+                    }
+            };
+                
+        
+        }
+        private void MouseOut(object sender, EventArgs e)
+        {
+            DescriptionLabel.Text = defaultDescription;
         }
     }
 }
