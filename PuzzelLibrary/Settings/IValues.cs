d6e35565069e14a2d314f714f1ce187200fba8b6
lewsa17@gmail.com <lewsa17@gmail.com>
2020-09-23 20:55:34 +0200

    Klasa do przechowywania wartości ustawień

---
+++ PuzzelLibrary/Settings/IValues.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace PuzzelLibrary.Settings
+{
+    public interface IValues
+    {
+        static bool HistoryLog { get; set; }
+        static long UserMaxLogs { get; set; }
+        static long CompMaxLogs { get; set; }
+        static bool CustomSource { get; set; }
+        static bool AutoOpenPort { get; set; }
+        static bool AutoUnlockFirewall { get; set; }
+        static bool SaveUserData { get; set; }
+
+    }
+}
---
+++ PuzzelLibrary/Settings/Values.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Xml;
+
+namespace PuzzelLibrary.Settings
+{
+   public class Values : IValues
+    {
+        public static bool HistoryLog { get; set; }
+        public static decimal UserMaxLogs { get; set; }
+        public static decimal CompMaxLogs { get; set; }
+        public static bool CustomSource { get; set; }
+        public static string CustomSourceData { get; set; }
+        public static bool AutoOpenPort { get; set; }
+        public static bool AutoUnlockFirewall { get; set; }
+        public static bool SaveUserData { get; set; }
+        public static string SessionDisconectShortcut { get; set; }
+
+        public static void Commit()
+        {
+            XmlWriter writer = null;
+            if (!System.IO.File.Exists("Settings.xml"))
+            {
+                writer = new SetSettings().CreateSettingsFile();
+            }
+            SetSettings.CreateSettingValues(writer);
+            SetSettings.CloseSettingsFile(writer);
+        }
+        public static void LoadValues()
+        {
+
+        }
+    }
+}
