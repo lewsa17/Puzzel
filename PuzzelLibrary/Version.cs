670501942d8d63d2459799c00ea013033be029bb
lewsa17@gmail.com <lewsa17@gmail.com>
2020-09-25 20:09:12 +0200

    Podbicie do 0.131

--- PuzzelLibrary/Version.cs
+++ PuzzelLibrary/Version.cs
@@ -3,7 +3,7 @@
     public static class Version
     {
         public static int Major { get => 0; }
-        public static int Minor { get => 130; }
+        public static int Minor { get => 131; }
         public static int Build { get => 0; }
         public static string GetVersion() => Major + "." + Minor + "." + Build;
     }
