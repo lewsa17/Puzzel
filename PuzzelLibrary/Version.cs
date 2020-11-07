959e4c9f39d769f75988a2749ddb41914854a2f4
lewsa17@gmail.com <lewsa17@gmail.com>
2020-11-07 21:47:59 +0100

    Automatyczna inkrementacja wersji na bazie lokalnych commitów

--- PuzzelLibrary/Version.cs
+++ PuzzelLibrary/Version.cs
@@ -1,12 +1,12 @@
-﻿namespace PuzzelLibrary
+namespace PuzzelLibrary
 {
     public static class Version
     {
         public static int Major { get => 0; }
         public static int Minor { get => 132; }
-        public static int Build { get => 225; }
-        public static string Hash { get => "2bfa0a5d"; }
-        public static System.DateTime BuildDate { get => System.DateTime.Parse("07.11.2020 13:58:28"); }
+        public static int Build { get => 226; }
+        public static string Hash { get => "9fb244a3"; }
+        public static System.DateTime BuildDate { get => System.DateTime.Parse("Sat Nov 7 14:04:25"); }
         public static string GetVersion() => Major + "." + Minor + "." + Build;
     }
 }
