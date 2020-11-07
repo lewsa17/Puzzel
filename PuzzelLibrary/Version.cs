2bfa0a5d5107c9a3b5c5a2ccacc9871e51928547
lewsa17@gmail.com <lewsa17@gmail.com>
2020-11-07 13:58:28 +0100

    Podbicie wersji

--- PuzzelLibrary/Version.cs
+++ PuzzelLibrary/Version.cs
@@ -3,7 +3,7 @@
     public static class Version
     {
         public static int Major { get => 0; }
-        public static int Minor { get => 131; }
+        public static int Minor { get => 132; }
         public static int Build { get => 216; }
         public static string Hash { get => "b8a73262"; }
         public static System.DateTime BuildDate { get => System.DateTime.Parse("26.09.2020 02:51:34"); }
