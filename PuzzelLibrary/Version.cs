9fb244a32310848fe680c420564493ea3e335d1c
lewsa17@gmail.com <lewsa17@gmail.com>
2020-11-07 14:04:25 +0100

    Zgodność o 1 commit wstecz

--- PuzzelLibrary/Version.cs
+++ PuzzelLibrary/Version.cs
@@ -4,9 +4,9 @@
     {
         public static int Major { get => 0; }
         public static int Minor { get => 132; }
-        public static int Build { get => 216; }
-        public static string Hash { get => "b8a73262"; }
-        public static System.DateTime BuildDate { get => System.DateTime.Parse("26.09.2020 02:51:34"); }
+        public static int Build { get => 225; }
+        public static string Hash { get => "2bfa0a5d"; }
+        public static System.DateTime BuildDate { get => System.DateTime.Parse("07.11.2020 13:58:28"); }
         public static string GetVersion() => Major + "." + Minor + "." + Build;
     }
 }
