ef42c59abef774049d4c94470202488910b5b6bf
lewsa17@gmail.com <lewsa17@gmail.com>
2020-11-12 20:48:53 +0100

    Revert "Zmiana wersji"
    This reverts commit bdedc7c3e360c1e27b606c24328d5435e96423ee.

--- Forms/External/LAPSui.cs
+++ Forms/External/LAPSui.cs
@@ -18,10 +18,7 @@ namespace Forms.External
         public void LoadPassword()
         {
             inputedcomputerName.Text = HostName;
-            var txt = CompPWD.GetPWD(HostName);
-            textPassword.Text = txt[0].ToString();
-            long text = Convert.ToInt64(txt[1]);
-            textPasswordExpires.Text = DateTime.FromFileTime(text).ToString();
+            textPassword.Text = CompPWD.GetPWD(HostName);
         }
         private void btnClose_Click(object sender, EventArgs e)
         {
@@ -30,12 +27,12 @@ namespace Forms.External
         }
         private void btnGetPwd(object sender, EventArgs e)
         {
-            LoadPassword();
+            textPassword.Text = CompPWD.GetPWD(inputedcomputerName.Text);
         }
 
         private void LAPSui_Load(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(HostName))
+            if (HostName != null)
             {
                 LoadPassword();
             }
