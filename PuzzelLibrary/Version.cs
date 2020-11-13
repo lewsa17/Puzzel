40e1e4b2d56ed7d82c63cca1ffefd6da98cca9b1
lewsa17@gmail.com <lewsa17@gmail.com>
2020-11-14 00:57:45 +0100

    Zaktualizowano i dodano brakujące bilbiloteki, podniesiono dotnet do wersji 5

--- PuzzelLibrary/Version.cs
+++ PuzzelLibrary/Version.cs
@@ -4,9 +4,9 @@ namespace PuzzelLibrary
     {
         public static int Major { get => 0; }
         public static int Minor { get => 134; }
-        public static int Build { get => 246; }
-        public static string Hash { get => "93fe9cb6"; }
-        public static System.DateTime BuildDate { get => System.DateTime.Parse("2020-11-13 23:38:43 +0100"); }
+        public static int Build { get => 248; }
+        public static string Hash { get => "2d534213"; }
+        public static System.DateTime BuildDate { get => System.DateTime.Parse("2020-11-14 00:42:38 +0100"); }
         public static string GetVersion() => Major + "." + Minor + "." + Build;
     }
 }
