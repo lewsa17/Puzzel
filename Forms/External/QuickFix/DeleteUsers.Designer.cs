37b94dc55e359417126ac964f728d6638ab0f445
lewsa17@gmail.com <lewsa17@gmail.com>
2020-11-27 21:37:24 +0100

    Usunięcie typów

--- Forms/External/QuickFix/DeleteUsers.Designer.cs
+++ Forms/External/QuickFix/DeleteUsers.Designer.cs
@@ -28,54 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DeleteUsers));
-            this.ComboBoxUsers = new System.Windows.Forms.ComboBox();
-            this.DeleteUserLabel = new System.Windows.Forms.Label();
-            this.BtnDeleteUsers = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new(typeof(DeleteUsers));
+            this.ComboBoxUsers = new();
+            this.DeleteUserLabel = new();
+            this.BtnDeleteUsers = new();
             this.SuspendLayout();
             // 
             // ComboBoxUsers
             // 
             this.ComboBoxUsers.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.ComboBoxUsers.FormattingEnabled = true;
-            this.ComboBoxUsers.Location = new System.Drawing.Point(71, 48);
+            this.ComboBoxUsers.Location = new(71, 48);
             this.ComboBoxUsers.Name = "ComboBoxUsers";
-            this.ComboBoxUsers.Size = new System.Drawing.Size(128, 21);
+            this.ComboBoxUsers.Size = new(128, 21);
             this.ComboBoxUsers.TabIndex = 0;
             // 
             // DeleteUserLabel
             // 
             this.DeleteUserLabel.AutoSize = true;
-            this.DeleteUserLabel.Location = new System.Drawing.Point(12, 23);
+            this.DeleteUserLabel.Location = new(12, 23);
             this.DeleteUserLabel.Name = "DeleteUserLabel";
-            this.DeleteUserLabel.Size = new System.Drawing.Size(249, 13);
+            this.DeleteUserLabel.Size = new(249, 13);
             this.DeleteUserLabel.TabIndex = 1;
             this.DeleteUserLabel.Text = "Wybierz użytkownika z listy którego chcesz usunąć";
             // 
             // BtnDeleteUsers
             // 
             this.BtnDeleteUsers.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.BtnDeleteUsers.Location = new System.Drawing.Point(71, 75);
+            this.BtnDeleteUsers.Location = new(71, 75);
             this.BtnDeleteUsers.Name = "BtnDeleteUsers";
-            this.BtnDeleteUsers.Size = new System.Drawing.Size(128, 24);
+            this.BtnDeleteUsers.Size = new(128, 24);
             this.BtnDeleteUsers.TabIndex = 2;
             this.BtnDeleteUsers.Text = "Usuń użytkownika";
             this.BtnDeleteUsers.UseVisualStyleBackColor = true;
-            this.BtnDeleteUsers.Click += new System.EventHandler(this.BtnDeleteUsers_Click);
+            this.BtnDeleteUsers.Click += new(this.BtnDeleteUsers_Click);
             // 
             // DeleteUsersForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(273, 103);
+            this.ClientSize = new(273, 103);
             this.Controls.Add(this.BtnDeleteUsers);
             this.Controls.Add(this.DeleteUserLabel);
             this.Controls.Add(this.ComboBoxUsers);
             this.Icon = global::Forms.Resources.Resources.Puzzel;
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(289, 142);
+            this.MaximumSize = new(289, 142);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(289, 142);
+            this.MinimumSize = new(289, 142);
             this.Name = "DeleteUsersForm";
             this.Text = "Usuwanie użytkowników";
             this.ResumeLayout(false);
--- Forms/External/QuickFix/Environment_Variable.Designer.cs
+++ Forms/External/QuickFix/Environment_Variable.Designer.cs
@@ -28,82 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EnvironmentVariable));
-            this.TextBoxNameVar = new System.Windows.Forms.TextBox();
-            this.TextBoxValueVar = new System.Windows.Forms.TextBox();
-            this.LabelTitleEnv = new System.Windows.Forms.Label();
-            this.LabelNameVar = new System.Windows.Forms.Label();
-            this.LabelValueVar = new System.Windows.Forms.Label();
-            this.BtnInputVar = new System.Windows.Forms.Button();
-            this.BtnCancel = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new(typeof(EnvironmentVariable));
+            this.TextBoxNameVar = new();
+            this.TextBoxValueVar = new();
+            this.LabelTitleEnv = new();
+            this.LabelNameVar = new();
+            this.LabelValueVar = new();
+            this.BtnInputVar = new();
+            this.BtnCancel = new();
             this.SuspendLayout();
             // 
             // TextBoxNameVar
             // 
-            this.TextBoxNameVar.Location = new System.Drawing.Point(108, 34);
+            this.TextBoxNameVar.Location = new(108, 34);
             this.TextBoxNameVar.Name = "TextBoxNameVar";
-            this.TextBoxNameVar.Size = new System.Drawing.Size(184, 20);
+            this.TextBoxNameVar.Size = new(184, 20);
             this.TextBoxNameVar.TabIndex = 0;
             // 
             // TextBoxValueVar
             // 
-            this.TextBoxValueVar.Location = new System.Drawing.Point(108, 60);
+            this.TextBoxValueVar.Location = new(108, 60);
             this.TextBoxValueVar.Name = "TextBoxValueVar";
-            this.TextBoxValueVar.Size = new System.Drawing.Size(184, 20);
+            this.TextBoxValueVar.Size = new(184, 20);
             this.TextBoxValueVar.TabIndex = 1;
             // 
             // LabelTitleEnv
             // 
             this.LabelTitleEnv.AutoSize = true;
-            this.LabelTitleEnv.Location = new System.Drawing.Point(12, 9);
+            this.LabelTitleEnv.Location = new(12, 9);
             this.LabelTitleEnv.Name = "LabelTitleEnv";
-            this.LabelTitleEnv.Size = new System.Drawing.Size(290, 13);
+            this.LabelTitleEnv.Size = new(290, 13);
             this.LabelTitleEnv.TabIndex = 2;
             this.LabelTitleEnv.Text = "Podaj nazwę zmiennej środowiskowej którą chcesz ustawić:";
             // 
             // LabelNameVar
             // 
             this.LabelNameVar.AutoSize = true;
-            this.LabelNameVar.Location = new System.Drawing.Point(18, 37);
+            this.LabelNameVar.Location = new(18, 37);
             this.LabelNameVar.Name = "LabelNameVar";
-            this.LabelNameVar.Size = new System.Drawing.Size(90, 13);
+            this.LabelNameVar.Size = new(90, 13);
             this.LabelNameVar.TabIndex = 3;
             this.LabelNameVar.Text = "Nazwa zmiennej: ";
             // 
             // LabelValueVar
             // 
             this.LabelValueVar.AutoSize = true;
-            this.LabelValueVar.Location = new System.Drawing.Point(11, 63);
+            this.LabelValueVar.Location = new(11, 63);
             this.LabelValueVar.Name = "LabelValueVar";
-            this.LabelValueVar.Size = new System.Drawing.Size(97, 13);
+            this.LabelValueVar.Size = new(97, 13);
             this.LabelValueVar.TabIndex = 4;
             this.LabelValueVar.Text = "Wartość zmiennej: ";
             // 
             // BtnInputVar
             // 
-            this.BtnInputVar.Location = new System.Drawing.Point(86, 89);
+            this.BtnInputVar.Location = new(86, 89);
             this.BtnInputVar.Name = "BtnInputVar";
-            this.BtnInputVar.Size = new System.Drawing.Size(75, 23);
+            this.BtnInputVar.Size = new(75, 23);
             this.BtnInputVar.TabIndex = 5;
             this.BtnInputVar.Text = "Wprowadź";
             this.BtnInputVar.UseVisualStyleBackColor = true;
-            this.BtnInputVar.Click += new System.EventHandler(this.BtnInputVar_Click);
+            this.BtnInputVar.Click += new(this.BtnInputVar_Click);
             // 
             // BtnCancel
             // 
-            this.BtnCancel.Location = new System.Drawing.Point(167, 89);
+            this.BtnCancel.Location = new(167, 89);
             this.BtnCancel.Name = "BtnCancel";
-            this.BtnCancel.Size = new System.Drawing.Size(75, 23);
+            this.BtnCancel.Size = new(75, 23);
             this.BtnCancel.TabIndex = 6;
             this.BtnCancel.Text = "Anuluj";
             this.BtnCancel.UseVisualStyleBackColor = true;
-            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            this.BtnCancel.Click += new(this.BtnCancel_Click);
             // 
             // Environment
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(311, 124);
+            this.ClientSize = new(311, 124);
             this.Controls.Add(this.BtnCancel);
             this.Controls.Add(this.BtnInputVar);
             this.Controls.Add(this.LabelValueVar);
@@ -113,9 +113,9 @@
             this.Controls.Add(this.TextBoxNameVar);
             this.Icon = global::Forms.Resources.Resources.Puzzel;
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(327, 163);
+            this.MaximumSize = new(327, 163);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(327, 163);
+            this.MinimumSize = new(327, 163);
             this.Name = "Environment";
             this.Text = "Zmienna środowiskowa";
             this.ResumeLayout(false);
