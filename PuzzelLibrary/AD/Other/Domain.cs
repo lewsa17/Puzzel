8f2a4c580ab883b4b4f85cfc9e25c4f04eb2cef6
lewsa17@gmail.com <lewsa17@gmail.com>
2020-12-04 20:06:30 +0100

    Rozszerzono wyszukiwanie kontrolerów w domenie poprzez wprowadzenie ręcznie jej nazwy

--- PuzzelLibrary/AD/Other/Domain.cs
+++ PuzzelLibrary/AD/Other/Domain.cs
@@ -26,5 +26,26 @@ namespace PuzzelLibrary.AD.Other
             return array;
         }
         public static string GetDomainName => System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().DomainName;
+
+        public static string[] GetDomainControllersByDomainName(string domainName)
+        {
+            DirectorySearcher search = new DirectorySearcher(new DirectoryEntry("LDAP://" + domainName));
+            //search.Filter = "(sAMAccountName=" + username + ")";
+            SearchResult search1 = search.FindOne();
+            //SearchResultCollection collection = search.FindAll();
+            object[] lines = (object[])search1.GetDirectoryEntry().Properties["msds-isdomainfor"].Value;
+            string table = null;
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(',');
+                table += words[1].Replace("CN=", "") + ",";
+            }
+
+            string[] array = null;
+            array = table.Split(',');
+            Array.Resize(ref array, array.Length - 1);
+
+            return array;
+        }
     }
 }
