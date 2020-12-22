712ec7524b5261b964076d537cdbf77373b0636f
lewsa17@gmail.com <lewsa17@gmail.com>
2020-12-22 15:25:59 +0100

    Zmiana wersji

--- PuzzelLibrary/Version.cs
+++ PuzzelLibrary/Version.cs
@@ -3,10 +3,10 @@ namespace PuzzelLibrary
     public static class Version
     {
         public static int Major { get => 0; }
-        public static int Minor { get => 142; }
-        public static int Build { get => 303; }
-        public static string Hash { get => "8be4a27f"; }
-        public static System.DateTime BuildDate { get => System.DateTime.Parse("2020-12-08 16:38:40 +0100"); }
+        public static int Minor { get => 143; }
+        public static int Build { get => 305; }
+        public static string Hash { get => "7a284411"; }
+        public static System.DateTime BuildDate { get => System.DateTime.Parse("2020-12-22 14:44:46 +0100"); }
         public static string GetVersion() => Major + "." + Minor + "." + Build;
     }
 }
