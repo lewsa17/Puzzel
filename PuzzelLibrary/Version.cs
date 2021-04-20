c651a893a29e42345e9ad417e81905fe67730bd7
lewsa17@gmail.com <lewsa17@gmail.com>
2021-04-21 00:30:40 +0200

    Aktualizacja pakietów zależnych

--- PuzzelLibrary/Version.cs
+++ PuzzelLibrary/Version.cs
@@ -4,9 +4,9 @@ namespace PuzzelLibrary
     {
         public static int Major { get => 0; }
         public static int Minor { get => 146; }
-        public static int Build { get => 329; }
-        public static string Hash { get => "9a2da357"; }
-        public static System.DateTime BuildDate { get => System.DateTime.Parse("2021-02-02 00:45:28 +0100"); }
+        public static int Build { get => 334; }
+        public static string Hash { get => "d646178f"; }
+        public static System.DateTime BuildDate { get => System.DateTime.Parse("2021-04-21 00:26:23 +0200"); }
         public static string GetVersion() => Major + "." + Minor + "." + Build;
     }
 }
