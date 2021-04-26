162763625a231af91f2ae4fb3b38fc08195bc18b
lewsa17@gmail.com <lewsa17@gmail.com>
2021-04-26 22:57:32 +0200

    Dodano właściwość zawierającą informację o statusie portu

---
+++ PuzzelLibrary/QuickFix/UnlockRPC.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using PuzzelLibrary.Registry;
+using System.Linq;
+
+namespace PuzzelLibrary.QuickFix
+{
+    public class UnlockRPC
+    {
+        public UnlockRPC(string HostName, Microsoft.Win32.RegistryHive mainCatalog, string subKey)
+        {
+            this.HostName = HostName;
+            this.mainCatalog = mainCatalog;
+            this.subKey = subKey;
+        }
+        private string HostName;
+        private Microsoft.Win32.RegistryHive mainCatalog;
+        private string subKey;
+        public bool IsOpen => isOpen();
+
+        public void RemoteUnlockPort()
+        {
+            new RegQuery().QueryKey(HostName, mainCatalog, subKey, "AllowRemoteRPC", "1", Microsoft.Win32.RegistryValueKind.DWord);
+        }
+        private bool isOpen()
+        {
+            var objects = new RegEnum().RegOpenRemoteSubKey(HostName, mainCatalog, subKey);
+            if (objects != null)
+                if (objects.GetValueNames().Contains("AllowRemoteRPC"))
+                {
+                    if (Convert.ToInt32(objects.GetValue("AllowRemoteRPC")) == 0)
+                        return true;
+                }
+            return false;
+        }
+    }
+}
