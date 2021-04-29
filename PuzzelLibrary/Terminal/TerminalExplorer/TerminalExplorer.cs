c738b54bef59047e936e7de37b83bee865b533ee
lewsa17@gmail.com <lewsa17@gmail.com>
2021-04-29 21:05:01 +0200

    Dodano wartości domyslne na wypadek braku w ustawieniach

--- PuzzelLibrary/Terminal/TerminalExplorer/TerminalExplorer.cs
+++ PuzzelLibrary/Terminal/TerminalExplorer/TerminalExplorer.cs
@@ -13,7 +13,9 @@ namespace PuzzelLibrary.Terminal
                 using (ITerminalServer server = new Explorer().GetRemoteServer(hostname))
                 {
                     server.Open();
-                    var Keys = Settings.Values.SessionDisconectShortcut.Split(" + ");
+                    string[] Keys = { "Control", "Multiply" };
+
+                    Keys = Settings.Values.SessionDisconectShortcut.Split('+');
                     var consoleKey = (ConsoleKey)TypeDescriptor.GetConverter(typeof(ConsoleKey)).ConvertFromString(Keys[1]);
                     var remoteControlHotkeyModifiers = (RemoteControlHotkeyModifiers)TypeDescriptor.GetConverter(typeof(RemoteControlHotkeyModifiers)).ConvertFromString(Keys[0]);
                     server.GetSession(sessionID).StartRemoteControl(consoleKey, remoteControlHotkeyModifiers);
