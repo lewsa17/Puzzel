ceff2ecd5250d65fa1f5c1b6e9dcc87cbe335de9
lewsa17@gmail.com <lewsa17@gmail.com>
2021-05-11 14:31:24 +0200

    Poprawiono aby przy wyszukiwaniu logów zmieniał się kursor również przy najechaniu na pole tekstowe

--- Forms/External/EventLogCollector.cs
+++ Forms/External/EventLogCollector.cs
@@ -85,6 +85,7 @@ namespace Forms.External
         private void FindButton_Click(object sender, System.EventArgs e)
         {
             this.Cursor = Cursors.WaitCursor;
+            TextLogView.Cursor = Cursors.WaitCursor;
             PuzzelLibrary.Debug.EventsCollector ec = new PuzzelLibrary.Debug.EventsCollector() { MaxCount = (int)LogCounter.Value };
             string dateFormat = "yyyy-MM-ddTHH:mm:ss.fffffffZ";
             var query = string.Format("*[System/TimeCreated/@SystemTime >= '{0}'] and *[System/TimeCreated/@SystemTime <= '{1}']",
@@ -124,6 +125,7 @@ namespace Forms.External
             }
             else TextLogView.Text = string.Format("Brak połączenia z {0}, serwer RPC jest niedostępny", LocationText.Text);
             this.Cursor = Cursors.Default;
+            TextLogView.Cursor = Cursors.Default;
         }
         private void InitializeTip()
         {
