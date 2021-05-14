d9db1845579c008e616c77d4db55375567a6117d
lewsa17@gmail.com <lewsa17@gmail.com>
2021-05-14 15:44:31 +0200

    Usunięto blokujące odwołania

--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -29,14 +29,7 @@ namespace Forms
         }
         private void CheckUpdate(object sender, EventArgs e)
         {
-            Updater.Updater updater = new Updater.Updater();
-            updater.Execute(new string[] {
-                PuzzelLibrary.Version.Major.ToString(),
-                PuzzelLibrary.Version.Minor.ToString(),
-                PuzzelLibrary.Version.Build.ToString(),
-                PuzzelLibrary.Version.Hash.ToString(),
-                PuzzelLibrary.Version.BuildDate.ToString()
-            });
+            PuzzelLibrary.ProcessExecutable.ProcExec.StartSimpleProcess("Updater.exe", "");
         }
         public static int ProgressBarValue = 0;
         public static int ProgressMax = 0;
