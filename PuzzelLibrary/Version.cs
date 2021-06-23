f398f63d52ece5284befecc4e9bc4bc667d7800c
lewsa17@gmail.com <lewsa17@gmail.com>
2021-06-23 15:57:47 +0200

    Nie wyświetlało nazw gdy były wpisywane z dużych liter

--- PuzzelLibrary/Version.cs
+++ PuzzelLibrary/Version.cs
@@ -4,9 +4,9 @@ namespace PuzzelLibrary
     {
         public static int Major { get => 0; }
         public static int Minor { get => 174; }
-        public static int Build { get => 567; }
-        public static string Hash { get => "045bc36d"; }
-        public static System.DateTime BuildDate { get => System.DateTime.Parse("2021-06-15 16:02:27 +0200"); }
+        public static int Build { get => 568; }
+        public static string Hash { get => "d90a0e2e"; }
+        public static System.DateTime BuildDate { get => System.DateTime.Parse("2021-06-15 16:03:15 +0200"); }
         public static string GetVersion() => Major + "." + Minor + "." + Build;
     }
 }
