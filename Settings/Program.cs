077036c1ee900344a8b784d43481977a3870f82a
lewsa17@gmail.com <lewsa17@gmail.com>
2021-09-02 15:45:16 +0200

    Ustawiono aby osobno aplikacja ustawień mogła uruchomić się tylko 1 raz

--- Settings/Program.cs
+++ Settings/Program.cs
@@ -19,7 +19,9 @@ namespace Settings
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             if (System.IO.File.Exists("PuzzelLibrary.dll"))
-                Application.Run(new SettingsForm(""));
+               if (System.Diagnostics.Process.GetProcessesByName(System.Diagnostics.Process.GetCurrentProcess().ProcessName).Length == 1)
+                    Application.Run(new SettingsForm(""));
+                else MessageBox.Show(new Form() { TopMost = true}, "Nie można kolejny raz włączyć ustawien", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
             else Application.Exit();
         }
     }
