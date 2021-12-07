ee4ca385102755d5a8240e220d23a7c07ace9200
lewsa17 <lewsa17@gmail.com>
2021-12-07 13:41:16 +0100

    Usunięto błąd który objawiał się gdy namespace był inny niż defaultowo ustawiony

--- PuzzelLibrary/Version.cs
+++ PuzzelLibrary/Version.cs
@@ -4,9 +4,9 @@ namespace PuzzelLibrary
     {
         public static int Major { get => 0; }
         public static int Minor { get => 192; }
-        public static int Build { get => 642; }
-        public static string Hash { get => "8e4faca0"; }
-        public static System.DateTime BuildDate { get => System.DateTime.Parse("2021-12-06 16:05:08 +0100"); }
+        public static int Build { get => 643; }
+        public static string Hash { get => "5fd717dc"; }
+        public static System.DateTime BuildDate { get => System.DateTime.Parse("2021-12-07 10:14:32 +0100"); }
         public static string GetVersion() => Major + "." + Minor + "." + Build;
     }
 }
