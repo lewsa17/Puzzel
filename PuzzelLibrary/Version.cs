f2bf0c3d4838fc6867c518dcbe348e89948c9353
lewsa17 <lewsa17@gmail.com>
2022-03-31 09:26:33 +0200

    Poprawiono odwołania do bibliotek

--- PuzzelLibrary/Version.cs
+++ PuzzelLibrary/Version.cs
@@ -4,9 +4,9 @@ namespace PuzzelLibrary
     {
         public static int Major { get => 0; }
         public static int Minor { get => 201; }
-        public static int Build { get => 665; }
-        public static string Hash { get => "24559957"; }
-        public static System.DateTime BuildDate { get => System.DateTime.Parse("2022-02-28 13:01:32 +0100"); }
+        public static int Build { get => 666; }
+        public static string Hash { get => "32b890e2"; }
+        public static System.DateTime BuildDate { get => System.DateTime.Parse("2022-02-28 13:41:47 +0100"); }
         public static string GetVersion() => Major + "." + Minor + "." + Build;
     }
 }
